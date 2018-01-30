2be631cbe2858c4c4522161784511f1ade81f763
John Stultz <john.stultz@linaro.org>
2018-01-30 12:32:35 -0800

    sync.sh: Update sync script to focus on HiKey
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- sync.sh
+++ sync.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 HOST= <<ADD HOST >>
-rsync -a --progress --inplace --delete $HOST:flo/ ./flo/
 rsync -a --exclude bug456 --exclude vendor  --progress --inplace --delete $HOST:hikey/ ./hikey/
 
 
